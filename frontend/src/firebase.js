18b3e0aaf7da7fe6e51ef91b662e5b8d1a4391e5
Ananya Singla <imananya07@gmail.com>
2026-03-31 22:23:48 +0530

    Privacy

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4J9sbkbBcOKL5y_m7vm3NtE-dQqn1k_M",
-  authDomain: "pillpal-4a695.firebaseapp.com",
-  databaseURL: "https://pillpal-4a695-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pillpal-4a695",
-  storageBucket: "pillpal-4a695.firebasestorage.app",
-  messagingSenderId: "184508444377",
-  appId: "1:184508444377:web:24ca22eb50d8896c8f98c8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getDatabase(app);
